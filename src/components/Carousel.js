310ef72782d9350063eff872d782e4ce668ea118
Jadrian Maxwell Abramovitch <jmabramovitch@uwaterloo.ca>
2022-02-03 20:51:56 -0500

    upgraded carousel, included swiping and auto swipe

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
 
 import "../styles/Carousel.css";
 
@@ -12,18 +13,41 @@ export const CarouselItem = ({ children, width }) => {
 
 const Carousel = ({ children }) => {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [paused, setPaused] = useState(false);
 
     const updateIndex = (newIndex) => {
         if (newIndex < 0) {
-            newIndex = 0;
-        } else if (newIndex >= React.Children.count(children)) {
             newIndex = React.Children.count(children) - 1;
+        } else if (newIndex >= React.Children.count(children)) {
+            newIndex = 0;
         }
         setActiveIndex(newIndex);
     }
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (!paused) {
+                updateIndex(activeIndex + 1); 
+            }
+        }, 5000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    })
+
+    const handlers = useSwipeable({
+        onSwipedLeft: () => updateIndex(activeIndex + 1),
+        onSwipedRight: () => updateIndex(activeIndex - 1)
+    });
+
     return (
-        <div className="carousel">
+        <div 
+            {...handlers}
+            className="carousel"
+            onMouseEnter={() => setPaused(true)}
+            onMouseLeave={() => setPaused(false)}
+        >
             <div className="carousel-inner"
             style={{ transform: `translateX(-${activeIndex * 100}%)`}}>
                 {React.Children.map(children, (child, index) => {
